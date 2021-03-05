8506f8445b44e9c127a9c94266e507ebfde040ed
hrisso <heather.risso@gmail.com>
2021-03-05 15:42:07 -0500

    create react app

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -46,6 +46,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :author, :description, :user_id)
+      params.require(:book).permit(:title, :author, :description, :img_url, :user_id)
     end
 end

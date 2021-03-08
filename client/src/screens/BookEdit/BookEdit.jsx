f649154f60090848580a9e24c9a448a2797ae0d7
hrisso <heather.risso@gmail.com>
2021-03-08 15:35:24 -0500

    edit book component

--- client/src/screens/BookEdit/BookEdit.jsx
+++ client/src/screens/BookEdit/BookEdit.jsx
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom';
 export default function BookEdit(props) {
   const [formData, setFormData] = useState({
     title: '',
+    author: '',
     img_url: '',
     description: ''
   });
@@ -17,6 +18,7 @@ export default function BookEdit(props) {
       const bookItem = books.find((book) => book.id === Number(id));
       setFormData({
         title: bookItem.title,
+        author: bookItem.author,
         img_url: bookItem.img_url,
         description: bookItem.description
       });
@@ -42,11 +44,38 @@ export default function BookEdit(props) {
   return (
     <form onSubmit={handleSubmit}>
       <h3>Edit Book</h3>
-      <label>Name:
+      <div className="image-container">
+        <img className="edit-product-image" src={formData.img_url} alt={formData.title} />
+      </div>
+      <label>Image Link:
+        <input
+          type='text'
+          name='img_url'
+          value={formData.img_url}
+          onChange={handleChange}
+        />
+      </label>
+      <label>Title:
         <input
           type='text'
           name='title'
           value={formData.title}
+          onChange={handleChange}
+        />
+      </label>
+      <label>Author:
+        <input
+          type='text'
+          name='author'
+          value={formData.author}
+          onChange={handleChange}
+        />
+      </label>
+      <label>Description:
+        <input
+          type='text'
+          name='description'
+          value={formData.description}
           onChange={handleChange}
         />
       </label>

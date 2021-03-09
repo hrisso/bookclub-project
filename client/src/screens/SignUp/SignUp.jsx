0286d69eee9a2c78bd1dc41225c925f2f8e871df
hrisso <heather.risso@gmail.com>
2021-03-09 16:58:33 -0500

    register styled

--- client/src/screens/SignUp/SignUp.jsx
+++ client/src/screens/SignUp/SignUp.jsx
@@ -20,39 +20,42 @@ export default function SignUp(props) {
   }
 
   return (
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      handleRegister(formData);
-    }}>
-      <h3>Register</h3>
-      <label>
-        Username:
-        <input
-          type='text'
-          name='username'
-          value={username}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Email:
-        <input
-          type='text'
-          name='email'
-          value={email}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Password:
-        <input
-          type='password'
-          name='password'
-          value={password}
-          onChange={handleChange}
-        />
-      </label>
-      <button>Submit</button>
-    </form>
+    <div className="signup-container">
+      <form className="sign-up-form"
+        onSubmit={(e) => {
+        e.preventDefault();
+        handleRegister(formData);
+        }}>
+        <h3>Register</h3>
+        <label>
+          Username:
+          <input
+            type='text'
+            name='username'
+            value={username}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type='text'
+            name='email'
+            value={email}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type='password'
+            name='password'
+            value={password}
+            onChange={handleChange}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
   )
 }
